16ed1940ee9f2b9100518a72a78164624b3f59c2
tuan <thanhtuanse16@gmail.com>
2023-11-23 16:53:53 +0700

    create img id - upload img to firebase storage successfully

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ import initWebRoutes from "./routes/web";
 import connection from "./config/connectDB";
 import initApiRoutes from "./routes/api";
 import bodyParser from 'body-parser';
+if (typeof window !== 'undefined') {
+    const { v4: uuidv4 } = require('uuid');
+    window.uuid = { v4: uuidv4 };
+}
+
 const app = express();
 
 //config view engine
